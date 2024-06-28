a2d0f7185dc947a4699b422105ef306794df9c52
taissilva2604 <taisinha2604@gmail.com>
2024-06-28 14:49:37 -0300

    Solução de exercícios

---
+++ pagina de login/script.js
@@ -0,0 +1,7 @@
+const loginForm = document.getElementById('loginForm');
+ 
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+   
+    window.location.href = 'file:///D:/Users/833420/OneDrive%20-%20SENAC%20em%20Minas%20-%20EDU/Área%20de%20Trabalho/CarrinhoDeComprasJS---II/index.html';
+});
